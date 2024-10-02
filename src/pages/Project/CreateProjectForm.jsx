c333e6056e622e99a3fe169226cbdd796913c381
manishraj27 <manish012raj@gmail.com>
2024-10-03 02:07:18 +0530

    Refactor form and select components

--- src/pages/Project/CreateProjectForm.jsx
+++ src/pages/Project/CreateProjectForm.jsx
@@ -1,8 +1,163 @@
+import { Button } from "@/components/ui/button";
+import { DialogClose } from "@/components/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useForm } from "react-hook-form";
+import { tags } from "../ProjectList/ProjectList";
+import { Cross1Icon } from "@radix-ui/react-icons";
 
 const CreateProjectForm = () => {
-  return (
-    <div>CreateProjectForm</div>
-  )
-}
+  const form = useForm({
+    // resolver:
+    defaultValues: {
+      name: "",
+      description: "",
+      category: "",
+      tags: ["javascript", "react"],
+    },
+  });
 
-export default CreateProjectForm
+  const onSubmit = (data) => {
+    console.log("create project: ", data);
+  };
+
+  const handleTagsChange = (newValue) => {
+
+    const currentTags= form.getValues("tags");
+
+    const updatedTags = currentTags.includes(newValue)
+    ? currentTags.filter(tag=>tag !== newValue)
+    : [...currentTags, newValue];
+    form.setValue("tags", updatedTags);
+  }
+
+
+  return (
+    <div>
+      <Form {...form}>
+        <form className="space-y-3" onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    {...field}
+                    type="text"
+                    className="border w-full border-gray-700 py-5 px-5"
+                    placeholder="Project Name..."
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="descriptions"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    {...field}
+                    type="text"
+                    className="border w-full border-gray-700 py-5 px-5"
+                    placeholder="Project Description..."
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="category"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Select
+                    defaultValue="fullstack"
+                    value={field.value}
+                    onValueChange={(value) => {
+                      field.onChange(value)
+                    }}
+                    //className="border w-full border-gray-700 py-5 px-5"
+                  >
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Select Category" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="fullstack">Full Stack</SelectItem>
+                      <SelectItem value="backend">Backend</SelectItem>
+                      <SelectItem value="frontend">Frontend</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage/>
+              </FormItem>
+            )}
+          />
+
+<FormField
+            control={form.control}
+            name="tags"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Select
+                    onValueChange={(value) => {
+                      handleTagsChange(value)
+                    }}
+
+                    //className="border w-full border-gray-700 py-5 px-5"
+                  >
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Tags" />
+                    </SelectTrigger>
+                    <SelectContent>
+                     {tags.map((item) => (
+                      <SelectItem key={item} value={item}>
+                        {item}
+                      </SelectItem>
+                    ))}
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <div className="flex gap-1 flex-wrap">
+                   {field.value.map((item)=>  <div key={item}
+                    onClick={()=> handleTagsChange(item)}
+                    className="cursor-pointer flex rounded-full items-center border gap-2 py-1 px-4"> 
+                      <span className="text-sm ">
+                        {item}
+                      </span>
+                      <Cross1Icon className="w-3 h-3"/>
+                    </div> )}
+                </div>
+                <FormMessage/>
+              </FormItem>
+            )}
+          />
+          <DialogClose>
+            {false ? (
+              <div>
+                <p>
+                  you can create only 3 projects with three plan, please upgrade
+                  your plan to create more projects
+                </p>
+              </div>
+            ) : (
+              <Button type="submit" className="w-full mt-5">
+                {" "}
+                Create Project
+              </Button>
+            )}
+          </DialogClose>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default CreateProjectForm;
