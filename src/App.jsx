7a93d739af919a82e30e7204786fc157c9e49196
manishraj27 <manish012raj@gmail.com>
2024-10-07 20:22:30 +0530

    routes defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Navbar from "./pages/Navbar/Navbar";
-import PortfolioPage from "./pages/Portfolio/Portfolio";
+import PortfolioPage from "./pages/Portfolio/PortfolioPage";
+import ProjectDetails from "./pages/ProjectDetails/ProjectDetails";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <PortfolioPage />
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/project-mangement" element={<Home />} />
+      <Route path="/portfolio" element={<PortfolioPage />} />
+      <Route path="/project/:id" element={<ProjectDetails />} />
+    </Routes>
     </>
   );
 }
